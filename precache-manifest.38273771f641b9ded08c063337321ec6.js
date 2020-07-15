cf76df2f4c07346d50d3db3eab6e50d59100b09a
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-07-15 11:00:46 +0930

    Updates

---
+++ precache-manifest.38273771f641b9ded08c063337321ec6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7671c8e7909707dab0611400c1478829",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ffbef20ca8ceccca16d6",
+    "url": "/static/css/main.5369e3e3.chunk.css"
+  },
+  {
+    "revision": "1ac635e5c5d1de9eb288",
+    "url": "/static/js/2.15ed9bb2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.15ed9bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffbef20ca8ceccca16d6",
+    "url": "/static/js/main.c7a6f2a5.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  }
+]);

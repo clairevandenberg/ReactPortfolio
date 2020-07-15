41b945f6ab72eb0e64c1b1d97f6ffe32176f1a80
Claire Vandenberg <vandenberg_claire@hotmail.com>
2020-07-15 19:55:42 +0930

    fixed nav bar

--- portfolio/src/components/Navbar/index.js
+++ portfolio/src/components/Navbar/index.js
@@ -1,51 +1,32 @@
-import React from "react";
+import React from 'react';
 import { Link } from "react-router-dom";
 import "./style.css";
 
-// Depending on the current path, this component sets the "active" class on the appropriate navigation link item
-function Navbar() {
+const Example = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <div>
-        <ul className="navbar-nav">
-            
-            <Link
-              to="/"
-              className={
-                window.location.pathname === "/" || window.location.pathname === "/claire"
-                  ? "nav-link active"
-                  : "nav-link"
-              }
-            >
-              Claire Vandenberg
-            </Link> 
-            
-            <Link
-              to="/about"
-              className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
-            >
-              About
-            </Link>
+    <div>
+      <nav>
+      <Link className="messier navbar-brand navbar navbar-expand-lg navbar-light bg-light" to="/">
+      CLAIRE VANDENBERG
+      </Link>
+
+      <Link className="navbar-brand navbar navbar-expand-lg navbar-light bg-light"  to="/about">
+        ABOUT
+      </Link>
+
+      <Link className="navbar-brand navbar navbar-expand-lg navbar-light bg-light"  to="/portfolio">
+        PORTFOLIO
+      </Link>
 
 
-            <Link
-              to="/portfolio"
-              className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
-            >
-              Portfolio
-            </Link>
-         
-         
-            <Link
-              to="/contact"
-              className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
-            >
-              Contact
-            </Link>
-        </ul>
-      </div>
-    </nav>
+      <Link className="navbar-brand navbar navbar-expand-lg navbar-light bg-light"  to="contact">
+        CONTACT
+      </Link>
+      
+      </nav>
+    </div>
   );
 }
 
-export default Navbar;
+export default Example;
+
